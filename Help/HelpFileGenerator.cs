f98f3f757ee2519637da4b70314a71ec2c97cf26
François Gasnier <fgasnier71@gmail.com>
2014-02-07 15:09:52 +0100

    Issue #2
    * Implemented xml document file schema.

---
+++ Help/HelpFileGenerator.cs
@@ -0,0 +1,18 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace Help
+{
+    public class HelpFileGenerator
+    {
+        #region Constructor
+        public HelpFileGenerator()
+        {
+        }
+        #endregion
+    }
+}
