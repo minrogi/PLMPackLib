d5ace213834ed14e6e5f5c1551067fa7ea612aa9
fgasnier71 <fgasnier71@gmail.com>
2014-09-10 09:16:56 +0200

    Issue #97
    * Removed splashpic_ocecanon resource file

--- PicParam/Properties/Resources.Designer.cs
+++ PicParam/Properties/Resources.Designer.cs
@@ -458,15 +458,5 @@ namespace PicParam.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap splashpic_ocecanon {
-            get {
-                object obj = ResourceManager.GetObject("splashpic_ocecanon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
