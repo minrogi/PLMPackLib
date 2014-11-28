379b5dac99bb8cded6bb38bb1d8958f9825e853d
fgasnier71 <fgasnier71@gmail.com>
2014-11-28 14:12:59 +0100

    Changed version 7.0.11.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("7.0.10.0")]
-[assembly: AssemblyFileVersion("7.0.10.0")]
+[assembly: AssemblyVersion("7.0.11.0")]
+[assembly: AssemblyFileVersion("7.0.11.0")]
